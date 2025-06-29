80e3e0557d5d21f78d57158deb428a50786dd3ac
Kali34432 <Kalianne22222@gmail.com>
2025-06-28 19:04:49 -0700

    Updated Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,12 +8,12 @@ interface LoginProps {
 }
 
 interface FormData {
-  email: string;
-  password: string;
-  confirmPassword: string;
-  firstName: string;
-  lastName: string;
-  acceptTerms: boolean;
+  #email: string;
+  #password: string;
+  #confirmPassword: string;
+  #firstName: string;
+  #lastName: string;
+  #acceptTerms: boolean;
 }
 
 interface ValidationErrors {
