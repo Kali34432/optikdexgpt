a693ca5493551b1e79db21de9f0ca140f1106823
Kali34432 <Kalianne22222@gmail.com>
2025-06-29 18:16:38 -0700

    Fix process is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -31,5 +31,9 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    global: 'window',
+  }
 });
